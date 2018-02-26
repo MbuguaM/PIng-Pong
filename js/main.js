cc9e12a6d87a8054c9cb20cd74526c573ccedfe5
Mbugua Munga <munga2mbugua@gmail.com>
2018-02-26 03:55:44 +0300

    fetched and stored user input in a global variable

--- js/main.js
+++ js/main.js
@@ -7,7 +7,9 @@ $(document).ready(function() {
 
      //search for the event
      $("#button").click(function(e){
-    
+           //fetch input from the input field
+           var input = parseInt($("#input").val());
+
 
    e.preventDefault();
  });
