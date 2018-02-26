a7cd8fc708fad15ae5fe7de31c257c8c2f2cb1ce
Mbugua Munga <munga2mbugua@gmail.com>
2018-02-26 03:53:28 +0300

    queried the button for intput through jquery

--- js/main.js
+++ js/main.js
@@ -1,7 +1,14 @@
-$(document).ready(function () {
 
+    
+//user interface logic
 
+// waiting for the document to load
+$(document).ready(function() {
 
+     //search for the event
+     $("#button").click(function(e){
+    
 
-
-})
+   e.preventDefault();
+ });
+});
