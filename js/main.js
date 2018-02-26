c8ee6361eefc0df6152c605577dc851114749bdc
Mbugua Munga <munga2mbugua@gmail.com>
2018-02-26 03:58:33 +0300

    bussiness logic: stored the numbers in a globall array

--- js/main.js
+++ js/main.js
@@ -11,6 +11,19 @@ $(document).ready(function() {
            var input = parseInt($("#input").val());
 
 
+
+
+       // bussiness logic
+     //array to store the numbers
+     var numbers=[];
+     //getting the numbers in the range of 1- user's input
+    function rangeFinder() {
+         for(var i =1;i<=input;i++){
+          numbers.push(i);
+         }
+    };
+    rangeFinder();    
+
    e.preventDefault();
  });
 });
